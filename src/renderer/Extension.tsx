cbc7324269da4c80a4479edb3141e158fb5bc0d7
KindaBrazy <kindofbrazy@gmail.com>
2026-06-20 19:51:15 +0330

    feat: refactor title bar widget rendering with reusable TestComp component and add base styles

--- src/renderer/Extension.tsx
+++ src/renderer/Extension.tsx
@@ -1,10 +1,10 @@
+import './index.css';
+
 import {ExtensionRendererApi} from '@lynx/plugins/extensions/types/api';
+
+import TestComp from './TestComp';
 
 export function InitialExtensions(lynxAPI: ExtensionRendererApi) {
   // Inject a basic widget into the title bar
-  lynxAPI.titleBar.addEnd(() => (
-    <div className="px-3 py-1 text-xs font-semibold rounded-full bg-primary/20 text-primary">
-      Hello from Template Extension!
-    </div>
-  ));
+  lynxAPI.titleBar.addEnd(TestComp);
 }
---
+++ src/renderer/TestComp.tsx
@@ -0,0 +1,9 @@
+export default function TestComp() {
+  return (
+    <div className="h-full py-1.5">
+      <div className="px-3 h-full text-xs font-semibold bg-foreground/10 content-center rounded-lg text-primary">
+        Hello from Template Extension!
+      </div>
+    </div>
+  );
+}
